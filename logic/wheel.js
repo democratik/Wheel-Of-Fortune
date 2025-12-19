f8261fc3ac4481c06d39595cc7b29617f5af17af
democratik <ilchenkodmytro522@gmail.com>
2025-12-19 20:08:27 +0100

    wheel update

--- logic/wheel.js
+++ logic/wheel.js
@@ -1,6 +1,8 @@
+// import Chart from "chart.js/auto";
+
 const refs = {
   wheel: document.querySelector(".wheel"),
-  message: document.querySelector(".massage-on-wheel")
+  message: document.querySelector(".massage-on-wheel"),
 };
 
 function textOnWheel() {
@@ -8,6 +10,45 @@ function textOnWheel() {
 }
 refs.wheel.addEventListener("click", textOnWheel);
 
+const DATA_COUNT = 5;
+const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
 
+const data = {
+  labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [1, 1, 1, 1, 1],
+      backgroundColor: ["red", "orange", "yellow", "green", "violet "],
+    },
+  ],
+};
 
+const config = {
+  type: "pie",
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Chart.js Pie Chart",
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  },
+};
 
+(async function () {
+  new Chart(document.getElementById("myChart"), {
+    ...config,
+  });
+})();
+
+console.log(Chart);
